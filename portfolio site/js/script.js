8c5bcddbecde9867f934c7132f4a0ae7d46fadb2
Laurens Duin <85798751+Laurens256@users.noreply.github.com>
2021-10-05 00:11:50 +0200

    Add files via upload

--- portfolio site/js/script.js
+++ portfolio site/js/script.js
@@ -1,32 +1,48 @@
-const homenav = document.querySelector("#homenav");
-const werknav = document.querySelector("#werknav");
-const contactnav = document.querySelector("#contactnav");
+const homenav = document.querySelectorAll(".homenav");
+const werknav = document.querySelectorAll(".werknav");
+const contactnav = document.querySelectorAll(".contactnav");
 const navItems = document.querySelectorAll(".navitems");
 let currentPageNav = homenav;
 
-const mainElement = document.querySelector("#pagescontainer");
+// const mainElement = document.querySelector("#pagescontainer");
 const homePagina = document.querySelector("#homepage");
 const mijnWerkPagina = document.querySelector("#mijnwerkpage");
 const contactPagina = document.querySelector("#contactpage");
 let currentPage = homePagina;
 
+const mobileNav = document.querySelector("#mobilenav");
+
 //change pages + markup
 function changePage(event) {
-    currentPageNav.classList.remove("active");
-    if  (event.target.id == "homenav") {
+	for (let i = 0; i < currentPageNav.length; i++) {
+		currentPageNav[i].classList.remove("active");
+	}
+    if (event.target.classList.contains("closenav")) {
+        mobileNav.classList.remove("mobilenavscrolldown");
+        mobileNav.classList.add("mobilenavscrollup");
+        setTimeout(function() {
+            mobileNav.classList.remove("mobilenavscrollup");
+        }, 1000);
+    }
+
+    if  (event.target.classList.contains("homenav")) {
         currentPageNav = homenav;
         currentPage = homePagina;
         document.title = "Home";
-    } else if (event.target.id == "werknav") {
+    } else if (event.target.classList.contains("werknav")) {
         currentPageNav = werknav;
         currentPage = mijnWerkPagina;
         document.title = "Mijn werk";
-    } else {
+    } else if (event.target.classList.contains("contactnav")){
         currentPageNav = contactnav;
         currentPage = contactPagina;
         document.title = "Contact";
+    } else if (event.target.id == "hamburgernav"){
+        mobileNav.classList.add("mobilenavscrolldown"); 
     }
-    currentPageNav.classList.add("active");
+    for (let i = 0; i < currentPageNav.length; i++) {
+		currentPageNav[i].classList.add("active");
+	}
     currentPage.scrollIntoView({block: "end", behavior: "smooth"});
 }
 
