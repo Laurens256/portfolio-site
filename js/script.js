ec2e00365ba676dd7d6dd804b4e7215813b331cc
Laurens Duin <85798751+Laurens256@users.noreply.github.com>
2021-12-15 21:37:59 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,36 +1,46 @@
+//paginas
 const homenav = document.querySelectorAll(".homenav");
 const werknav = document.querySelectorAll(".werknav");
 const contactnav = document.querySelectorAll(".contactnav");
+
+//mobile nav
 const navItems = document.querySelectorAll(".navitems");
+const navToggleButtons = document.querySelectorAll(".hamburger");
+const darkModeToggle = document.querySelector("#mobilenav ul li:last-of-type button");
+
+
 let currentPageNav = homenav;
+
 
 const mainElement = document.querySelector("main");
 const sectionElement = document.querySelectorAll("section");
-const homePagina = document.querySelector("#homepage");
-const mijnWerkPagina = document.querySelector("#mijnwerkpage");
-const contactPagina = document.querySelector("#contactpage");
-
-const mobileNav = document.querySelector("#mobilenav");
+// const homePagina = document.querySelector("#homepage");
+// const mijnWerkPagina = document.querySelector("#mijnwerkpage");
+// const contactPagina = document.querySelector("#contactpage");
 
 const bodyElement = document.querySelector("body");
 
-//change pages + markup
+const rootElement = document.querySelector(":root");
+let currentTheme = localStorage.getItem("currentTheme");
+
+
+function toggleNav() {
+    bodyElement.classList.toggle("activenav");
+}
+
+
+
+//change pages + active style
 function changePage(event) {
-    let scrollWidth = window.innerWidth || document.documentElement.clientWidth || 
+    const scrollWidth = window.innerWidth || document.documentElement.clientWidth || 
     document.body.clientWidth;
 
+    //highlight actieve pagina
 	for (let i = 0; i < currentPageNav.length; i++) {
 		currentPageNav[i].classList.remove("active");
 	}
-    if (event.target.classList.contains("closenav")) {
-        mobileNav.classList.remove("mobilenavscrolldown");
-        mobileNav.classList.add("mobilenavscrollup");
-        setTimeout(function() {
-            mobileNav.classList.remove("mobilenavscrollup");
-        }, 1000);
-    }
 
-    if  (event.target.classList.contains("homenav")) {
+    if (event.target.classList.contains("homenav")) {
         currentPageNav = homenav;
         mainElement.scrollTo(0, 0);
         document.title = "Home";
@@ -42,23 +52,59 @@ function changePage(event) {
         currentPageNav = contactnav;
         mainElement.scrollTo(scrollWidth*2, 0);
         document.title = "Contact";
-    } else if (event.target.id == "hamburgernav"){
-        mobileNav.classList.add("mobilenavscrolldown");
-    }
+    } 
+
     for (let i = 0; i < currentPageNav.length; i++) {
 		currentPageNav[i].classList.add("active");
 	}
-    if (!event.target.classList.contains("resetscroll")) {
+
+    if (!event.target.classList.contains("noscrollreset")) {
     for (let i = 0; i < sectionElement.length; i++) {
 		sectionElement[i].scrollTo(0, 0);
     }
     }
 }
 
-// contactform
 
 
+//darkmode toggle
+function toggleDarkMode(event) {
+    //pagina load geeft als event undefined, alleen dan kijken naar localstorage/systeemvoorkeur
+    if (event == undefined) {
+        if (window.matchMedia('(prefers-color-scheme: light)').matches && currentTheme == null) {
+            rootElement.classList.add("lightmode");
+        } else if (currentTheme == "light") {
+            rootElement.classList.add("lightmode");
+        }
+        return;
+    }
+
+    if (event.target.tagName == "BUTTON" || event.target.tagName == "SPAN") {
+        rootElement.classList.toggle("lightmode");
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        rootElement.classList.add("lightmode");
+    } else {
+        rootElement.classList.remove("lightmode");
+    }
+    
+    if (rootElement.classList.contains("lightmode")) {
+        localStorage.setItem("currentTheme", "light");
+    } else {
+        localStorage.setItem("currentTheme", "dark");
+    }
+}
+
+
+
+toggleDarkMode();
 
 for (let i = 0; i < navItems.length; i++) {
 	navItems[i].addEventListener("click", changePage);
 }
+
+for (let i = 0; i < navToggleButtons.length; i++) {
+	navToggleButtons[i].addEventListener("click", toggleNav);
+}
+
+window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", toggleDarkMode);
+darkModeToggle.addEventListener("click", toggleDarkMode);
