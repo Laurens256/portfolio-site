0be195b21b062e05345a49c2b4ba3ec352a7b8eb
Laurens Duin <85798751+Laurens256@users.noreply.github.com>
2022-03-08 18:55:09 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
 const bodyElement = document.querySelector("body");
+const hamburgerToggle = document.querySelector("header nav button");
 
 const h1Letters = document.querySelectorAll(".index section:first-of-type h1 span");
 
-const hamburgerToggle = document.querySelector("header nav button");
+const mijnwerkIndex = document.querySelectorAll(
+	".index section:nth-of-type(4) ul:nth-of-type(2) li"
+);
+
+//log tabbed element (testing)
+// document.addEventListener('focusin', function() {
+//   console.log('focused: ', document.activeElement)
+// }, true);
 
 const toggleNav = () => {
-  bodyElement.classList.toggle("openhamburger");
+	bodyElement.classList.toggle("openhamburger");
 
-  if (bodyElement.classList.contains("openhamburger") && window.matchMedia("(max-width: 768px)").matches) {
-    bodyElement.style.overflowY = "hidden";
-  } else {
-    bodyElement.style.overflowY = "auto";
-  }
+	if (
+		bodyElement.classList.contains("openhamburger") &&
+		window.matchMedia("(max-width: 768px)").matches
+	) {
+		bodyElement.style.overflowY = "hidden";
+	} else {
+		bodyElement.style.overflowY = "auto";
+	}
 };
 
 //spring animatie voor h1
 for (let i = 0; i < h1Letters.length; i++) {
-  h1Letters[i].addEventListener("animationend", function () {
-    h1Letters[i].classList.remove("dance");
-  });
+	h1Letters[i].addEventListener("animationend", function () {
+		h1Letters[i].classList.remove("dance");
+	});
 
-  h1Letters[i].addEventListener("mouseover", function () {
-    h1Letters[i].classList.add("dance");
-  });
+	h1Letters[i].addEventListener("mouseover", function () {
+		h1Letters[i].classList.add("dance");
+	});
 }
 
 hamburgerToggle.addEventListener("click", toggleNav);
